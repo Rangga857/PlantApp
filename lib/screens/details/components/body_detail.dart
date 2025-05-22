7fc26036755d37d3161dac8211da479a0f266590
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:58:32 +0700

    Tambah textbutton Buy Now dan Description di halaman detail produk

--- lib/screens/details/components/body_detail.dart
+++ lib/screens/details/components/body_detail.dart
@@ -16,6 +16,38 @@ class BodyDetail extends StatelessWidget {
           ImageAndIcons(size: size),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
           SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    backgroundColor: kPrimaryColor,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text("Description"),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
