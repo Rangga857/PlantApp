12a5913f51793000581b9ae7f5b83ccb0eaf705b
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:34:07 +0700

    Tambah GestureDetector untuk tappable image card

--- lib/screens/home/components/feature_plant_card.dart
+++ lib/screens/home/components/feature_plant_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../constrants.dart';
+
 class FeaturePlantCard extends StatelessWidget {
   final String image;
   final VoidCallback press;
@@ -13,6 +15,24 @@ class FeaturePlantCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return const Placeholder();
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
   }
 }
