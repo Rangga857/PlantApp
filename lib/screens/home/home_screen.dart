9f1ebcea49ebb3acaf39f189206e8d66194bc83e
Rangga857 <azharrangga28@gmail.com>
2025-05-22 23:22:11 +0700

    setup appbar body dan bottomnavbar pada homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp_mobile/components/my_bottom_nav_bar.dart';
+import 'package:plantapp_mobile/screens/home/components/body_home.dart';
 
 import '../../constrants.dart';
 
@@ -8,7 +10,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: BodyHome(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
   }
 }
 AppBar buildAppBar() {
