3550f3d0bfbab43464363db5af4564b54f89f33f
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:44:13 +0700

    add 2 TextSpan pada titleandprice

--- lib/screens/details/components/title_and_price.dart
+++ lib/screens/details/components/title_and_price.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../constrants.dart';
+
 class TitleAndPrice extends StatelessWidget {
   final String title, country;
   final int price;
@@ -13,6 +15,32 @@ class TitleAndPrice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "$title\n",
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: kTextColor, 
+                    fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: country,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
