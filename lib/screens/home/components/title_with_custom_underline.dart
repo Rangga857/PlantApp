69c276b25ed9607442c514baef9dc4aa1cfeb400
Rangga857 <azharrangga28@gmail.com>
2025-05-22 21:56:00 +0700

    deklarasi properti text dengan tipe string

---
+++ lib/screens/home/components/title_with_custom_underline.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  final String text;
+
+  const TitleWithCustomUnderline(
+    {super.key,
+    required this.text
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
