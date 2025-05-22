511c276ffbf5d4bb73e1f94e51f8f1cdb4cddccd
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:24:29 +0700

    add parameter size

---
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HeaderWithSearchbox extends StatelessWidget {
+  final Size size;
+  const HeaderWithSearchbox({
+    super.key,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
