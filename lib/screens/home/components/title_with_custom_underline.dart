e91f2d4a8273556caf2c460c57be1d83035ba54a
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:01:39 +0700

    add layout padding pada TitleWithCustomUnderline

--- lib/screens/home/components/title_with_custom_underline.dart
+++ lib/screens/home/components/title_with_custom_underline.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp_mobile/constrants.dart';
 
 class TitleWithCustomUnderline extends StatelessWidget {
   final String text;
@@ -10,6 +11,22 @@ class TitleWithCustomUnderline extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ],
+      ) ,
+    );
   }
 }
