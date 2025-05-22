7aedcc36c14f9acf2618298b8b5dc6a5e7a551ab
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:18:35 +0700

    use singlechildscroll dan panggil class RecomendPlantCard

---
+++ lib/screens/home/components/recomend_plants.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp_mobile/screens/home/components/recomend_plant_card.dart';
+
+class RecomendPlants extends StatelessWidget {
+  const RecomendPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children:<Widget>[
+          RecomendPlantCard(
+            image : 'assets/images/image_1.png',
+            title: 'Samantha',
+            country: 'Russia',
+            price: 440,
+            press: (){
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen()
+                )
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: 'assets/images/image_2.png', 
+            title: 'Angelica',
+            country: 'Russia', 
+            price: 440,
+            press: (){
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => const DetailsScreen()
+                )
+              );
+            }
+          ),
+          RecomendPlantCard(
+            image: 'assets/images/image_3.png',
+            title: 'Samantha',
+            country: 'Russia',
+            price: 440,
+            press: (){}
+          )
+        ],
+      ),
+    );
+  }
+}
