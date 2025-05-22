91158d5e087d0fbb5fb24df5aec33c9d193cef53
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:02:17 +0700

    add positioned dan panggil kPrimaryColor

--- lib/screens/home/components/title_with_custom_underline.dart
+++ lib/screens/home/components/title_with_custom_underline.dart
@@ -25,6 +25,15 @@ class TitleWithCustomUnderline extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+            margin: EdgeInsets.only(right: kDefaultPadding / 4),
+            height: 7,
+            color: kPrimaryColor.withOpacity(0.2),
+          ),)
         ],
       ) ,
     );
