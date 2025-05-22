11b75e422ced281e27f7fa4f6a58144762abaa62
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:49:49 +0700

    add parameter size

---
+++ lib/screens/details/components/image_and_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImageAndIcons extends StatelessWidget {
+  final Size size;
+
+  const ImageAndIcons({super.key,
+    required this.size
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
