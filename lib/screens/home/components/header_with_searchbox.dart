b0b553544e1e92d3c7c02931067f7b82264db961
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:27:03 +0700

    add stack untuk layout bertumpuk

--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../constrants.dart';
+
 class HeaderWithSearchbox extends StatelessWidget {
   final Size size;
   const HeaderWithSearchbox({
@@ -9,6 +11,39 @@ class HeaderWithSearchbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Hi Rangga!',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                Image.asset("assets/images/logo.png")
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
