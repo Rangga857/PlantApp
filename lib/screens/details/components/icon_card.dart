0ce9a9a6465fbcb363cfb21d5e8eb8e559c39e8d
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:46:02 +0700

    add parameter icon

---
+++ lib/screens/details/components/icon_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IconCard extends StatelessWidget {
+  final String icon;
+  const IconCard({super.key,
+    required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
