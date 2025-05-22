8835f0206fa8cffe1861e041e568366795caac56
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:03:55 +0700

    panggil class TitleWithCustomUnderline

--- lib/screens/home/components/title_with_more_btn.dart
+++ lib/screens/home/components/title_with_more_btn.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp_mobile/constrants.dart';
+import 'package:plantapp_mobile/screens/home/components/title_with_custom_underline.dart';
 
 class TitleWithMoreBtn extends StatelessWidget {
   final String title;
   final VoidCallback press;
-  
+
   const TitleWithMoreBtn(
     {super.key,
     required this.title,
@@ -13,6 +15,12 @@ class TitleWithMoreBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding
+    (padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+    child: Row(
+      children: <Widget>[
+        TitleWithCustomUnderline(text: title),
+      ],)
+    );
   }
 }
