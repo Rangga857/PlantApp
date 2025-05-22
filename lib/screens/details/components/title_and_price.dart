dd7348d5c32eabee57a87631e3b64812863ea141
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:42:48 +0700

    add parameter(title, country, dan price)

---
+++ lib/screens/details/components/title_and_price.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TitleAndPrice extends StatelessWidget {
+  final String title, country;
+  final int price;
+
+  const TitleAndPrice(
+    {super.key,
+    required this.title,
+    required this.country,
+    required this.price
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
