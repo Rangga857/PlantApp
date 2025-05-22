3c10eb8397d547626e81c65586b90e9bf19eb733
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:30:41 +0700

    add parameter(image dan press)

---
+++ lib/screens/home/components/feature_plant_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FeaturePlantCard extends StatelessWidget {
+  final String image;
+  final VoidCallback press;
+
+  const FeaturePlantCard({
+    super.key,
+    required this.image,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
