2a470753267d634e5dcf695a97586585308f3a2e
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:07:10 +0700

    deklarasi properti(imamge, title, country, dan price) pada PlantCard

---
+++ lib/screens/home/components/recomend_plant_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RecomendPlantCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String country;
+  final int price;
+  final VoidCallback press;
+
+  const RecomendPlantCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
