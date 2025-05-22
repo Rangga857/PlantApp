c122b56aadd1311503044e24952d8b72adf26bd3
Rangga857 <azharrangga28@gmail.com>
2025-05-22 23:20:48 +0700

    create function buildAppBar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constrants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+AppBar buildAppBar() {
+  return AppBar(
+    backgroundColor: kPrimaryColor ,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset("assets/icons/menu.svg"),
+      onPressed: () {},
+    ),
+  );
+}
