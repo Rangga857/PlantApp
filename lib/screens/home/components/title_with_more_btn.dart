3669e624d312b54ebf2471e495b37225493b127d
Rangga857 <azharrangga28@gmail.com>
2025-05-22 21:54:05 +0700

    deklarasi properti(title dan press)

---
+++ lib/screens/home/components/title_with_more_btn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  final String title;
+  final VoidCallback press;
+  
+  const TitleWithMoreBtn(
+    {super.key,
+    required this.title,
+    required this.press,
+    }
+    );
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
