cc3dce6c29f3d3841c36b9ab57cf476c4e02e644
Rangga857 <azharrangga28@gmail.com>
2025-05-22 22:35:57 +0700

    add singlescroll dan panggil class featureplantcard

---
+++ lib/screens/home/components/featurred_plants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp_mobile/screens/home/components/feature_plant_card.dart';
+
+class FeaturredPlants extends StatelessWidget {
+  const FeaturredPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
